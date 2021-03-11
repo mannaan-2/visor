a1675aaf7a90a242a2f4c05ecf710aee5fcea3a8
Abdul Manan <abdul.mannaan@gmail.com>
2021-03-11 22:30:37 +0000

    added required config

--- src/Visor/Visor.Api/Program.cs
+++ src/Visor/Visor.Api/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -18,7 +19,28 @@ namespace Visor.Api
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                //configHost.SetBasePath(Directory.GetCurrentDirectory());
+                //configHost.AddJsonFile("hostsettings.json", optional: true);
+                //configHost.AddEnvironmentVariables(prefix: "PREFIX_");
+                //configHost.AddCommandLine(args);
+                config.SetBasePath(hostingContext.HostingEnvironment.ContentRootPath);
+                config.AddJsonFile("AppIdentitySettings.json", optional: false, reloadOnChange: true);
+                config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                config.AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                config.AddJsonFile("Secrets.json", optional: true, reloadOnChange: true);
+                config.AddJsonFile($"Secrets.{hostingContext.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                config.AddEnvironmentVariables();
+                config.AddCommandLine(args);
+            })
+            .ConfigureLogging((hostingContext, logging) =>
+            {
+                logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                logging.AddConsole();
+                logging.AddDebug();
+               // logging.AddAzureWebAppDiagnostics();
+            }).ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
